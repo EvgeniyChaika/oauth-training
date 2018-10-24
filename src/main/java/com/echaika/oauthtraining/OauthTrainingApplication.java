5d06ffd97361c038f65d1a215800f41c22bd6c00
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2018-10-24 17:48:39 +0300

    Refactoring code

--- src/main/java/com/echaika/oauthtraining/OauthTrainingApplication.java
+++ src/main/java/com/echaika/oauthtraining/OauthTrainingApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OauthTrainingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OauthTrainingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OauthTrainingApplication.class, args);
+    }
 }
--- src/main/java/com/echaika/oauthtraining/config/OAuth2AuthorizationServer.java
+++ src/main/java/com/echaika/oauthtraining/config/OAuth2AuthorizationServer.java
@@ -2,13 +2,11 @@ package com.echaika.oauthtraining.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @Configuration
-@PropertySource(name = "applicationProperties", value = "application.yaml")
 @EnableAuthorizationServer
 public class OAuth2AuthorizationServer extends AuthorizationServerConfigurerAdapter {
 
